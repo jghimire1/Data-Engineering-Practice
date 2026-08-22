952863d5a0879c7647a351b9c75a5afbe1ff6b89
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-08-22 09:48:22 -0600

    Add ROW_NUMBER function example for ranking fruits
    Added example of ROW_NUMBER function to rank fruit names.

--- SQL practice 7 _RANK.sql
+++ SQL practice 7 _RANK.sql
@@ -4,3 +4,8 @@ create table fruits (name varchar(255));
 insert into fruits values ('apple'), ('apple'), ('orange'), ('grapes'), ('grapes'), ('watermelon');
 
 select * from fruits;
+
+--ROW_Number Function 
+-- This is the simplest of all to understand. This function will just rank all selected rows in ascending order, 
+--regardless of the values that were selects. 
+SELECT name, ROW_NUMBER() OVER(ORDER BY name) from fruits;
