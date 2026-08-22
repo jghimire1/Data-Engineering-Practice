7afd7cadea4e3b7386073843385445a34b92d405
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-08-22 09:44:36 -0600

    Update and rename SQL practice 7 _RANK to SQL practice 7 _RANK.sql

---
+++ SQL practice 7 _RANK.sql
@@ -0,0 +1,6 @@
+-- RANK Function Practice 
+create table fruits (name varchar(255));
+
+insert into fruits values ('apple'), ('apple'), ('orange'), ('grapes'), ('grapes'), ('watermelon');
+
+select * from fruits;
