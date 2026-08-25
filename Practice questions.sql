4627ced588d5d64b85c7a5f5880c2b677f2c84d2
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-08-25 12:22:11 -0600

    Add SQL queries and create customer and student tables
    Added SQL queries for employee bonuses and customer referrals, and created customer and student tables.

--- Practice questions.sql
+++ Practice questions.sql
@@ -28,3 +28,36 @@ values
 select * from employee;
 
 select * from bonus;
+
+--Write an SQL query to report the name and bonus amount of each employee with a bonus less than 1000.
+--Return the result table in any order
+ select employee.name, bonus.bonus from employee
+ left join bonus on bonus.empID = employee.empID 
+ where bonus < 1000 
+ or bonus is null; 
+
+ --creating table customer 
+ create table customer(
+id int, 
+name varchar, 
+referee_id int
+);
+
+--inserting values to customer table 
+insert into customer (id, name, referee_id)
+VALUES 
+(1, 'Will', null),
+(2, 'Jane', null),
+(3, 'Alex', 2),
+(4, 'Bill', null),
+(5, 'Zack', 1),
+(1, 'Mark', 2); 
+
+--Write an SQL query to report the names of the customer that are not referred by the customer with id = 2.
+Select name from customer where referee_id != 2 or referee_id is null;
+
+--creating student table 
+create table student (
+student varchar,
+class varchar
+); 
