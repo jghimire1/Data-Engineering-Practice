2f4b04b6c5019898d22b791ff0f3289b17ccac82
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-08-27 17:29:07 -0600

    Add Python practice script with variable examples
    This file demonstrates basic Python concepts including variable assignment, casting, type checking, and case sensitivity.

---
+++ Python Practice1.py
@@ -0,0 +1,46 @@
+ # Python practice
+x = 5
+y = "John"
+
+print(x)
+print(y)
+
+# casting
+# if you want to specify the data type of variable, this can be done with casting.
+
+x = str(3) # x will be '3'
+y = int(3) # y will be 3
+z = float(3) # z will be 3.0
+
+print (x)
+print (y)
+print(z)
+
+# Get the type
+print(type(x))
+print(type(y))
+
+# single or Double quotes?
+x = "John"
+# is the same.
+x = 'John'
+
+# Case Sensitivity
+# Variable names are case-sensitive.
+
+a = 4
+A = "Sally" # A will not overwrite a
+print(a, A)
+
+# Many Values to Multiple variables
+x,y,z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+# One value to multiple variables
+# And you can assign the same value to multiple variables in one line:
+x=y=z= "Orange"
+print(x)
+print(y)
+print(z)
