237018291835f2fb530c136c64212cea151d96dd
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-08-27 17:30:12 -0600

    Add global variable example in Python Practice1.py
    Added explanation of global variables and a function to demonstrate their usage.

--- Python Practice1.py
+++ Python Practice1.py
@@ -64,4 +64,12 @@ y = "is"
 z = "awesome"
 print(x,y,z)
 
+# Global Variables
+# Variables that are created outside of a function (as in all of the examples above) are known as global variables.
+# Global variables can be used by everyone, both inside of functions and outside.
+x = "Awesome!"
+def myfunc():
+    print("Python is " + x)
+myfunc()
+
 
