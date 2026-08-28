e9e09c307edd724851cb710b3f34819f7f891f90
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-08-28 08:35:10 -0600

    Enhance Python practice with string examples
    Added examples of multiline strings, string slicing, and negative indexing in Python.

--- python practice - data types.py
+++ python practice - data types.py
@@ -20,3 +20,38 @@ x = range(6) #---> range
 x = {"name": "John", "age": 36} #---> dict
 x = {"apple", "banana", "cherry"} # ---> set
 x = True # ---> boolean
+
+#Multiline String
+a = """My name is janardan. I am learning python. 
+Practice makes perfect so keep practicing everyday. """
+
+print(a)
+
+print("-----------")
+#python slicing strings
+# We can return a range of characters by using the slice syntax.
+# Specify the start index and the end index, separated by a colon, to return a part of the string.
+
+#Get the characters from position 2 to position 5 (not included):
+b = "Hello world!"
+print(b[2:5])
+
+print("------+-----")
+
+# Get the characters from the start to position 5(not included):
+print(b[:5])
+
+print("------++-----")
+#get the characters from position 2, and all the way to end:
+print(b[2:])
+print("------+++-----")
+
+# Negative Indexing
+#Use negative indexes to start the slice from the end of the string:
+#Get characters from "o" in "World! (position -5)
+# To, but not included: "d" in "World! (position -2) not included:
+
+b = "Hello world!"
+print(b[-5:-2])
+
+print("------+++*-----")
