da7966ee86179374f66af816c1973b090a148260
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-08-28 08:34:32 -0600

    Document built-in data types with examples
    Added examples of built-in data types in Python.

---
+++ python practice - data types.py
@@ -0,0 +1,22 @@
+# Built in Data types
+# Text type -- str
+# Numeric type -- int, float
+# sequence type -- list, tuple
+# mapping type -- dict
+# set type -- set
+# boolean type -- bool
+
+# Examples
+x = "Hello World" # data type --> String
+x = 20 #---> int
+x = 20.5 # ---> float
+x = 1j # datatype ----> complex, python uniquely uses the letter j (or J) to denote the imaginary unit,
+        # # it is built in numeric type usd to represent complex numbers
+
+x = ["apple", "banana", "cherry"] # ---> list
+x = ("apple", "banana", "cherry") #---> tuple
+
+x = range(6) #---> range
+x = {"name": "John", "age": 36} #---> dict
+x = {"apple", "banana", "cherry"} # ---> set
+x = True # ---> boolean
