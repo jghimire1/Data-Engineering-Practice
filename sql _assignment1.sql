2a3d7612a93c18276c729352e407de010f74ba02
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-08-29 09:50:56 -0600

    Add sample data to SQL tables
    Inserted sample data into customers1, products1, and orders1 tables.

--- sql _assignment1.sql
+++ sql _assignment1.sql
@@ -22,3 +22,30 @@ product_id int,
 quantity int, 
 order_date date
 ); 
+
+-- Inserting sample data to the previously created tables 
+INSERT INTO customers1 VALUES 
+(1, 'Rahus', 'Mumbai'),
+(2,'Anjali', 'Delhi'),
+(3, 'Aman', 'Pune'),
+(4, 'Sneha', 'Banglore'),
+(5, 'Karan', 'Mumbai');
+
+INSERT INTO products1 VALUES
+(101, 'Laptop', 'Electronics', 70000),
+(102, 'Phone', 'Electronics', 40000),
+(103, 'Chair', 'Furniture', 5000),
+(104, 'Desk', 'Furniture', 12000),
+(105, 'Headphones', 'Electronics', 3000);
+
+INSERT INTO orders1 VALUES
+(1001, 1, 101, 1, '2026-01-10'),
+(1002, 1, 105, 2, '2026-01-11'),
+(1003, 2, 102, 1, '2026-01-12'),
+(1004, 3, 103, 4, '2026-01-13'),
+(1005, 4, 104, 1, '2026-01-14'),
+(1006, 5, 101, 1, '2026-01-15'),
+(1007, 2, 105, 3, '2026-01-16'),
+(1008, 3, 102, 1, '2026-01-17'),
+(1009, 1, 103, 2, '2026-01-18'),
+(1010, 4, 105, 5, '2026-01-19');
