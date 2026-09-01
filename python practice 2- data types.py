30af0959adf56725f2e885cb7a006277efb811da
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-09-01 12:20:27 -0600

    Implement conditionals and boolean checks
    Added conditional message printing and boolean evaluations.

--- python practice 2- data types.py
+++ python practice 2- data types.py
@@ -116,3 +116,32 @@ print(10==9)
 print( 10 < 9)
 
 
+# Print a message based on whether the condition is true or false:
+a = 200
+b = 33
+
+print()
+print("******#******")
+
+if b >a:
+    print("b is greater than a.")
+else:
+    print("b is not greater than a.")
+
+print()
+print("******#******")
+# Most Values are True
+# ALmost any value is evaluated to True if it has some sort of content.
+# Any string is True, except empty strings.
+# Any number is True, except 0
+# Any list, tuple, set, and dictionary are True, except empty ones.
+# The following will return true:
+
+bool("abc")
+print(bool("abc"))
+
+bool(123)
+print(bool(123))
+
+bool(["apple", "cherry", "banana"])
+print(bool(["apple", "cherry", "banana"]))
