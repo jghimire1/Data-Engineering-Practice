935a416471e75ba742ea03e94db7ab1437f34714
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-09-01 15:06:57 -0600

    Enhance Python script with logical operators section
    Added examples and explanations for logical operators.

--- Python_Operator_Practice 3.py
+++ Python_Operator_Practice 3.py
@@ -42,3 +42,14 @@ print("Python comparison Operators------------------")
 x = 5
 y = 3
 print(x==y)
+
+
+print()
+print("Python Logical Operators-------------")
+# Logical operators are used to combine conditional statements:
+# Operator	Description	                                Example
+# and -->	Returns True if both statements are true  -->	x < 5 and  x < 10
+# or -->	Returns True if one of the statements is true -->	x < 5 or x < 4
+# not -->	Reverse the result, returns False if the result is true	--> not(x < 5 and x < 10)
+x = 5
+print(x> 3 and x < 10)
