62df56529b3d0ac15aec79eb22f8c8d144b9a649
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-09-01 15:05:41 -0600

    Document Python operators in Python_Operator_Practice 3.py
    Added explanations for various Python operators including arithmetic, assignment, and bitwise operators.

---
+++ Python_Operator_Practice 3.py
@@ -0,0 +1,28 @@
+# Python Operator
+# Python Arithmetic Operators
+# +	Addition ---->	x + y
+# -	Subtraction ---->	x - y
+# *	Multiplication ---->	x * y
+# /	Division ---->	x / y
+# %	Modulus	---> x % y
+# **	Exponentiation ---->	x ** y
+# //	Floor division	----> x // y
+
+print(" Python Assignment operators-------------")
+# Assignment operators are used to assign the values to variables.
+# Operator	Example	Same As
+#assign or equal  =	 -->   x = 5	-->     x = 5
+# add: += -->	x += 3  -->	    x = x + 3
+# subtract: -= -->	x -= 3 -->	    x = x - 3
+# multiply: *= -->	x *= 3 -->	    x = x * 3
+# Divide: /= -->	x /= 3 -->	    x = x / 3
+# Modules: %= -->	x %= 3	-->     x = x % 3
+# floor divide: //= -->	x //= 3	-->     x = x // 3
+# Exponent: **= -->	x **= 3 -->	    x = x ** 3
+# BITWISE AND --> &= -->	x &= 3 -->	    x = x & 3 # SETS EACH BIT TO 1 IF BOTH BITS ARE 1
+# BITWISE OR--> |= -->	x |= 3 -->	    x = x | 3 # sets each bit to 1 if at least one of the bits is 1.
+# BITWISE XOR--> ^= -->	x ^= 3 -->	    x = x ^ 3 # SETS EACH BIT TO 1 IF ONLY ONE OF THE BITS IS 1.
+# Right shift --> >>= -->	x >>= 3 -->	    x = x >> 3 #shifts the binary bits of the left variable to the right by the
+                        #  number of positions specified on the right. This effectively halves the number for each shift.
+# Left Shift --> <<= -->	x <<= 3 -->	    x = x << 3 # shifts the binary bits to the left, adding zeros on the
+                                    # right. This effectively doubles the nuber for each shift.
