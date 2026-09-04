6746c34ee320577f4e4d75e12fc562a505208e09
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-09-04 12:20:27 -0600

    Implement string and list duplicate removal and max finder
    Added functions to remove duplicates from a string and a list, and to find the second largest number in a list.

--- Python Assessment1.py
+++ Python Assessment1.py
@@ -49,3 +49,39 @@ for x, frequency in frequency.items():
     print(x,":",frequency, end =",")
 
 print()
+
+print("Q5: Remove duplicate characters from a string while preserving order.")
+
+org_str = "programming" #input ("Enter your string: ")
+
+result = ""
+
+for x in org_str:
+    if x not in result:
+        result += x
+print(result)
+
+print("Q6: Remove duplicates from a list without using set().")
+input_list = [1,2,2,3,4,4]
+removed_duplicates = []
+for num in input_list:
+     if num not in removed_duplicates:
+         removed_duplicates.append(num)
+print(removed_duplicates)
+
+print("Q7: Find the second largest number in the list.")
+number_list = [10,20,5,30,25]
+max_value = number_list[0]
+
+#finding max value
+for num in number_list:
+    if num > max_value:
+        max_value = num
+
+#finding second max value
+second_max = None
+for num in number_list:
+    if num != max_value:
+        if second_max is None or num > second_max:
+            second_max = num
+print(second_max)
