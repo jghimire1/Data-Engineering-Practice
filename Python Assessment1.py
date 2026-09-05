5f5977c53b8e4ca387af8c2b18178e57f20fb87c
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-09-05 12:53:15 -0600

    Implement frequency count for list elements
    Added functionality to count frequency of elements in a list using a dictionary.

--- Python Assessment1.py
+++ Python Assessment1.py
@@ -116,3 +116,14 @@ for n in lst1:
         intersection_value.append(n)
 print("Intersection of two list is: ", intersection_value)
 
+print("Q11: Count the frequency of elements in a list using dictionary.")
+lst_given = [1,2,2,3,3,3]
+
+dict = {}
+for x in lst_given:
+    if x in dict:
+        dict[x] = dict[x] + 1
+    else:
+        dict[x] = 1
+print(dict)
+
