774c8d8d32f270f2d91c53559de798c3c12636e7
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-09-17 14:13:51 -0600

    Update hive business case - medical data.sql

--- hive business case - medical data.sql
+++ hive business case - medical data.sql
@@ -20,3 +20,12 @@ partitioned by  (visit_date DATE, region STRING) ;
 -- creating non bucketed table  
 create table input_medical_visit (visit_id INT, patient_id INT, region STRING, visit_date DATE, diagnosis STRING, 
 treatment STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' STORED AS TEXTFILE; 
+
+
+-- load data to non - bucketed input_medical visit 
+load data local inpath 'medical_visit.csv' into table input_medical_visit; 
+
+-- creating bucketed and partitioning table 
+create table medical_visit (visit_id INT,  patient_id INT, diagnosis STRING, treatment STRING) 
+partitioned by  (visit_date DATE, region STRING) clustered by (diagnosis, patient_id ) into 10 buckets 
+ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' ;
