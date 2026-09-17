7b9e0212ae95e975480f039a4bc83b2e1108c46c
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-09-16 22:02:47 -0600

    Update hive business case-sales data .sql

--- hive business case-sales data .sql
+++ hive business case-sales data .sql
@@ -15,3 +15,14 @@ insert into non_par_sales_data VALUES
 -- creating partition table partitioned on sale_date, and country
 CREATE TABLE partition_sales_data(sale_id INT, product_id INT, product_category STRING, customer_id INT, 
 sale_amount FLOAT, region STRING) partitioned by (sale_date Date, country STRING); 
+
+-- copying data from non partition table to partition table
+insert overwrite table partition_sales_data partition (sale_date, country) select sale_id, product_id, product_category, customer_id, 
+sale_amount, region, sale_date, country from non_par_sales_data; 
+
+-- creating non-bucketed table 
+CREATE TABLE non_buck_sales_data(sale_id INT, product_id INT, product_category STRING, customer_id INT, sale_amount FLOAT,
+sale_date Date, country STRING, region STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' STORED AS TEXTFILE ; 
+
+-- loading data from local to non bucketed table 
+load data local inpath 'sales_data' into table non_buck_sales_data; 
