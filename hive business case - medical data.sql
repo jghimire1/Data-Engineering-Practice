f9655fb83b2bb2d4c719303343e054b56a0807db
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-09-17 14:13:11 -0600

    Create input table for medical visits and insert data
    Created a table for medical visits and inserted sample data.

--- hive business case - medical data.sql
+++ hive business case - medical data.sql
@@ -1,1 +1,14 @@
+-- creating input table for medical visists table 
+
+create table input_medical_visit (visit_id INT, patient_id INT, region STRING, visit_date DATE, diagnosis STRING, treatment STRING); 
+
+-- loading value to the input medical visit 
+insert into input_medical_visit VALUES 
+(1,101,'North','2023-09-01','Hypertension','Medication'),
+(2,102,'South','2023-09-02','Diabetes','Insulin'),
+(3,103,'East','2023-09-03','Flu','Antiviral'),
+(4,104,'North','2023-09-04','Asthma','Inhaler'),
+(5,105,'South','2023-09-05','Hypertension','Medication');
+
+
 
