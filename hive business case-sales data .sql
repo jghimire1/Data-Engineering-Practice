0f20bf2578b558cef551a4ded0aeb5806035a961
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-09-16 22:02:25 -0600

    Add non-partitioned and partitioned sales data tables
    Created a non-partitioned sales data table and inserted sample data. Also defined a partitioned sales data table based on sale_date and country.

--- hive business case-sales data .sql
+++ hive business case-sales data .sql
@@ -1,1 +1,17 @@
+- creating non partition table 
 
+CREATE TABLE non_par_sales_data(sale_id INT, product_id INT, product_category STRING, customer_id INT, 
+sale_amount FLOAT, sale_date Date, country STRING, region STRING); 
+
+
+insert into non_par_sales_data VALUES 
+(1,101,'Electronics',2001,500.00,'2023-08-01','US','North America'),
+(2,102,'Electronics',2002,300.00,'2023-08-01','US','North America'),
+(3,103,'Furniture',2003,700.00,'2023-08-02','UK','Europe'),
+(4,104,'Furniture',2004,800.00,'2023-08-03','UK','Europe'),
+(5, 105,'Clothing',2005,200.00,'2023-08-03','IN','Asia'),
+(6,106,'Clothing',2006,600.00,'2023-08-03','IN','Asia');
+
+-- creating partition table partitioned on sale_date, and country
+CREATE TABLE partition_sales_data(sale_id INT, product_id INT, product_category STRING, customer_id INT, 
+sale_amount FLOAT, region STRING) partitioned by (sale_date Date, country STRING); 
