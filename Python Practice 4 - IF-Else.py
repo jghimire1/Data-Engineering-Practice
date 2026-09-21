eec5aaf5b590329f9c40cb69c3b6ba161a5b4c1b
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-09-20 18:12:31 -0600

    Enhance conditional statements examples in Python
    Added examples of if-else, AND, and OR conditions.

--- Python Practice 4 - IF-Else.py
+++ Python Practice 4 - IF-Else.py
@@ -24,3 +24,33 @@ if b > a:
 elif a == b:
     print("a and b are equal.")
 
+
+print("Else-------------")
+# The else keyword catches anything which isn't caught by the preceding conditions.
+
+a = 200
+b = 33
+
+if b > a:
+    print("b is greater than a.")
+elif a ==b:
+    print("a and b are equal. ")
+else:
+    print("a is greater than b.")
+
+print("AND--------------")
+# The AND keyword is a logical operator, and is used to combine conditional statements.
+
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+    print("Both conditions are True.")
+
+print("OR----------------")
+# The OR keyword is a logical operator, and is used to combine conditional statements:
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+    print("At least one of the conditions is True.")
