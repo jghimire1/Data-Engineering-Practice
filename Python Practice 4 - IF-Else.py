ba201d0280e5bc74f3210d416b4770260eb93ec1
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-09-20 18:12:05 -0600

    Enhance Python script with comments on conditions
    Added comments explaining logical conditions and IF statements in Python.

--- Python Practice 4 - IF-Else.py
+++ Python Practice 4 - IF-Else.py
@@ -1,1 +1,26 @@
+# Python Conditions and IF Statements
+# Python Supports the usual logical conditions from mathematics:
+#	Equals: a == b
+#	Not Equals: a != b
+#	Less than: a < b
+#	Less than or equal to: a <= b
+#	Greater than: a > b
+#	Greater than or equal to: a >= b
+print("IF--------------------")
+a = 33
+b = 200
+if b > a:
+    print("b is greater than b.")
+
+
+print("Elif---------------")
+# The elif keyword is python's way of saying
+# "if previous conditions were not true, then try this condition".
+
+a = 33
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal.")
 
