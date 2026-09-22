c138a4755ea3a89b621211003e7540db9240b587
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-09-22 13:30:16 -0600

    Add comments on lambda function usage
    Added comments explaining lambda functions and their usage.

--- Python Practice 7 - Lambda Function.py
+++ Python Practice 7 - Lambda Function.py
@@ -1,1 +1,8 @@
+# Lambda Function
+# A lambda function is a small anonymous function.
+# A lambda function can take any number of arguments, but can only have one expression.
 
+# Addition
+print("Add 10 to argument a, and return the result:")
+x = lambda a:a+10
+print(x(5))
