0a355fae070182cc5e994c1a0f153cc40b8770d5
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-09-22 13:31:10 -0600

    Add lambda function examples and explanations
    Added examples demonstrating the use of lambda functions in Python, including a summarization function and a function that returns a lambda for multiplication.

--- Python Practice 7 - Lambda Function.py
+++ Python Practice 7 - Lambda Function.py
@@ -16,3 +16,17 @@ print(x(5,6))
 print("Summarise argument a,b, and c and return the result. ")
 x = lambda a,b,c : a+b+c
 print(x(5,6,2))
+
+#Note
+# The power of lambda is better shown when you use them as an anonymous function inside another function.
+# Let's say you have a function definition that takes one argument, and that argument will be multiplied with an unknown
+# number.
+print("Lambda - advancement ")
+def myfunc(n):
+    return lambda a:a*n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11))
