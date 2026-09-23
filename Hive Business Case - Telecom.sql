28102d71383ebe28bfb401980f17f8b70aa6b33c
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-09-23 17:58:48 -0600

    Add call data tables for telecom analysis
    Created three tables for call data: non-partitioned, partitioned, and non-bucketed.

--- Hive Business Case - Telecom.sql
+++ Hive Business Case - Telecom.sql
@@ -1,1 +1,23 @@
+-- creating non-partition call data table 
+create table npt_call_data (
+call_id INT, 
+customer_id INT, 
+call_duration FLOAT, 
+region STRING, 
+call_date DATE); 
 
+-- Creating partition call data table 
+
+create table pt_call_data (
+ call_id INT, 
+customer_id INT, 
+call_duration FLOAT 
+) partitioned by (call_date DATE,region STRING);
+
+-- creating non-bucketed table 
+create table nbct_call_data (
+call_id INT, 
+customer_id INT, 
+call_duration FLOAT, 
+region STRING, 
+call_date DATE) ROW FORMAT DELIMITED FIELDS TERMINATED BY "," stored as TEXTFILE ;
