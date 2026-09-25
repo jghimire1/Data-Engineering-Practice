e95f610b0a2ce6f4d3bcb9de49c994aaadcf4009
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-09-25 09:34:23 -0600

    Add SparkSession setup and import functions

--- In class exercise - 09.24.26- main_pyspark_functions.py
+++ In class exercise - 09.24.26- main_pyspark_functions.py
@@ -1,1 +1,19 @@
 
+from pyspark.sql import SparkSession
+
+from Get_Joins import join_function
+from get_distinctRows import get_distinct_rows
+from get_groupby_aggregate import groupby_aggregate_function
+from get_order_sortBy import sort_orderBy_Function
+
+if __name__ == '__main__':
+    spark: SparkSession = SparkSession.builder.master("local[1]").appName("bootcamp.com").getOrCreate()
+
+#get_distinct_rows(spark)
+
+#sort_orderBy_Function(spark)
+
+# groupby_aggregate_function(spark)
+join_function(spark)
+
+ 
