1e107ebd1e0b9cd531fd70845585caab6e1c5cba
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-09-25 09:36:04 -0600

    Implement get_distinct_rows function with PySpark
    Added a function to create a DataFrame and display distinct rows.

--- In class exercise - 09.24.26-get_distinctRows.py
+++ In class exercise - 09.24.26-get_distinctRows.py
@@ -1,1 +1,29 @@
+import pyspark
 
+from pyspark.sql.functions import expr
+
+def get_distinct_rows(spark):
+    data = [("James", "Sales", 3000), \
+            ("Michael", "Sales", 4600), \
+            ("Robert", "Sales", 4100), \
+            ("Maria", "Finance", 3000), \
+            ("James", "Sales", 3000), \
+            ("Scott", "Finance", 3300), \
+            ("Jen", "Finance", 3900), \
+            ("Jeff", "Marketing", 3000), \
+            ("Kumar", "Marketing", 2000), \
+            ("Saif", "Sales", 4100) \
+            ]
+    columns = ["employee_name", "department", "salary"]
+    df = spark.createDataFrame(data=data, schema=columns)
+    df.printSchema()
+    df.show(truncate=False)
+
+    distinctDF = df.distinct()
+    print("Distinct count: " + str(distinctDF.count()))
+    distinctDF.show(truncate=False)
+
+  
+
+
+ 
