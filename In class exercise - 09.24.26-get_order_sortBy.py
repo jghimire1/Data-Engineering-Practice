cccdfd3e2552fb9ee89d5a9fafcba451d298259f
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-09-25 09:37:58 -0600

    Implement sort_orderBy_Function to create DataFrame
    Added a function to create a DataFrame with employee data.

--- In class exercise - 09.24.26-get_order_sortBy.py
+++ In class exercise - 09.24.26-get_order_sortBy.py
@@ -1,1 +1,23 @@
+from os import truncate
 
+from pyspark.sql.functions import col
+
+
+def sort_orderBy_Function(spark):
+    simpleData = [("James", "Sales", "NY", 90000, 34, 10000), \
+                  ("Michael", "Sales", "NY", 86000, 56, 20000), \
+                  ("Robert", "Sales", "CA", 81000, 30, 23000), \
+                  ("Maria", "Finance", "CA", 90000, 24, 23000), \
+                  ("Raman", "Finance", "CA", 99000, 40, 24000), \
+                  ("Scott", "Finance", "NY", 83000, 36, 19000), \
+                  ("Jen", "Finance", "NY", 79000, 53, 15000), \
+                  ("Jeff", "Marketing", "CA", 80000, 25, 18000), \
+                  ("Kumar", "Marketing", "NY", 91000, 50, 21000) \
+                  ]
+    columns = ["employee_name", "department", "state", "salary", "age", "bonus"]
+    df = spark.createDataFrame(data=simpleData, schema=columns)
+    #df.printSchema()
+    #df.show(truncate=False)
+
+   
+ 
