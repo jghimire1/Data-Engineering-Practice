c6392a58645e6ea63a018e2185a1fc4afacd3c49
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-09-24 21:06:42 -0600

    Initialize SparkSession and create number RDD
    Added SparkSession initialization and RDD creation with sample data.

--- In Class Exercise - 09.23.26 - main.py
+++ In Class Exercise - 09.23.26 - main.py
@@ -1,1 +1,21 @@
+from pyspark.sql import SparkSession
+#from pyspark.sql.types import StructType,StructField, StringType, IntegerType
 
+from enforce_schema import enforce_schema
+from nested_schema import nested_schema
+
+
+def number_rdd():
+    global data
+    data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    rdd = spark.sparkContext.parallelize(data)
+    print(rdd.count())
+    print(rdd.collect())
+
+
+
+if __name__ == '__main__':
+    spark: SparkSession = SparkSession.builder.master("local[1]").appName("bootcamp.com").getOrCreate()
+
+#enforce_schema(spark)
+nested_schema(spark)
